fd4a4b13fbc5f30292f68204c8a8e45f472a01ea
ayabarakat3 <ayabarakat14@gmail.com>
2024-12-02 17:40:34 -0500

    update

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const ctx = document.getElementById('barChart');
-new Chart(
-    ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red","Blue", "Yellow"],
-            datasets: [
-                {
-                    label: 'Votes',
-                    data: [33,333,3],
-                    backgroundColor: ['red','blue','yellow'],
-                    borderColor: ['black']
-                }
-            ]
-        }
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow"],
+        datasets: [
+            {
+                label: 'Votes',
+                data: [33, 333, 3],
+                backgroundColor: ['red', 'blue', 'yellow'],
+                borderColor: ['black'],
+                borderWidth: 1
+            }
+        ]
     }
-)
+});
