d12edfc6f3e6d353661654a4f6b603bca9ee706e
ayabarakat3 <ayabarakat14@gmail.com>
2024-12-02 17:51:47 -0500

    updated colors

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const ctx = document.getElementById('barChart');
 new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["BabyPink", "Pink", "DarkPink", 'Red'],
+        labels: ["Maroon", "Olive", "Navy", 'Silver'],
         datasets: [
             {
                 label: 'Votes',
                 data: [373, 333, 353, 363],
-                backgroundColor: ['babyPink', 'pink', 'darkPink', 'red'],
+                backgroundColor: ['maroon', 'olive', 'navy', 'silver'],
                 borderColor: ['black'],
                 borderWidth: 1
             }
